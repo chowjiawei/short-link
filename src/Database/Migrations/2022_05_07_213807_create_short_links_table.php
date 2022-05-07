0866d781f4e3dff4586e824180b17212261a654a
chowjiawei <1084186870@qq.com>
2022-05-07 14:33:37 +0800

    修复明显异常

--- src/Database/Migrations/2022_05_07_213807_create_short_links_table.php
+++ src/Database/Migrations/2022_05_07_213807_create_short_links_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRedirectsTable extends Migration
+class CreateShortLinksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class CreateRedirectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('redirects');
+        Schema::dropIfExists('short_links');
     }
 }
--- src/Providers/ShortLinkServiceProvider.php
+++ src/Providers/ShortLinkServiceProvider.php
@@ -14,7 +14,7 @@ class ShortLinkServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__ . '/../Config/short-link.php' => config_path('short-link.php'),
-            __DIR__ . '/../Database/Migrations/2022_05_07_213807_create_redirects_table.php' => database_path('Migrations/2022_05_07_213807_create_redirects_table.php'),
+            __DIR__ . '/../Database/Migrations/2022_05_07_213807_create_short_links_table' => database_path('Migrations/2022_05_07_213807_create_short_links_table'),
         ]);
     }
 }
